31c2012eff410110fe4d8985d8d1a68996184eff
prashantalhat1406 <Janfeb01>
2015-06-17 12:14:03 +0530

    removed warnings

--- app/src/main/java/com/example/prashant/dentist/patientTabs.java
+++ app/src/main/java/com/example/prashant/dentist/patientTabs.java
@@ -2,7 +2,6 @@ package com.example.prashant.dentist;
 
 import android.app.TabActivity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
